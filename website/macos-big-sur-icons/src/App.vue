e0c48e6d033b18a9649be2d3d01d0000fc037c3e
elrumo <elrumo97@me.com>
2020-08-03 23:46:42 +0200

    New website with all icons, still needs a few things done before 'lanuch'

--- website/macos-big-sur-icons/src/App.vue
+++ website/macos-big-sur-icons/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+} 
 </style>
