0543a4b7cc56df26785524dcc20daf3b5a71991d
elrumo <elrumo97@me.com>
2020-08-04 20:57:23 +0200

    Search functionality added

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -39,12 +39,12 @@
 
     <!-- Icon Secion -->
     <section class="content-wrapper">
-
+      <!-- Search bar -->
       <div class="main-search-wrapper">
         <div class="search-bg"></div>
-        <div class="coral-Form coral-Form--vertical m-auto main-search" style="max-width:300px;">
+        <div class="m-auto main-search" style="max-width:300px;">
           <div class="shadow main-border-radius">
-            <input v-model="search" type="text" is="coral-textfield" class="_coral-Search-input _coral-Textfield" name="name" aria-label="text input">
+            <input v-model="searchString" type="text"  class="_coral-Search-input _coral-Textfield" name="name" aria-label="text input">
             <svg class="icon" id="spectrum-css-icon-Magnifier" viewBox="0 0 16 16"><path d="M15.77 14.71l-4.534-4.535a6.014 6.014 0 1 0-1.06 1.06l4.533 4.535a.75.75 0 1 0 1.061-1.06zM6.5 11A4.5 4.5 0 1 1 11 6.5 4.505 4.505 0 0 1 6.5 11z"></path></svg>
           </div>
         </div>
@@ -52,7 +52,8 @@
       </div>
   
       <div class="icon-list-area p-t-50 p-b-50">
-        <a v-for="icon in iconList" :key="icon.name" :href="icon.url" class="card-wrapper shadow">
+        <a v-for="icon in filteredList" :key="icon.name" :href="icon.url" class="card-wrapper shadow">
+        <!-- <a v-for="icon in iconList" :key="icon.name" :href="icon.url" class="card-wrapper shadow"> -->
           <div class="card-img-wrapper">
             <img loading="lazy" class="w-full" :src="icon.img" alt="">
           </div>
@@ -89,7 +90,8 @@ export default {
   data(){
     return{
       iconList:[],
-      search: ""
+      searchString: "",
+      iconsToShow: []
     }
   },
 
@@ -101,20 +103,21 @@ export default {
     getIconsArray(){
       var list = []
       let parent = this
-
       fetch('https://raw.githubusercontent.com/elrumo/macOS_Big_Sur_icons_replacements/master/icns.txt')
         .then(response => response.text())
         .then((data) => {
           list = data.split(", ")
 
           for(let icon in list){
-            let iconName = list[icon]
+            let id = list[icon]
+            let iconName = id.replace("_", " ")
             let itemObj = {
               name: iconName,
+              id: id,
               url:
-                    "https://github.com/elrumo/macOS-Big-Sur-icons-replacements/raw/master/icons/"+iconName+".icns",
+                    "https://github.com/elrumo/macOS-Big-Sur-icons-replacements/raw/master/icons/"+id+".icns",
               img: 
-                    "https://raw.githubusercontent.com/elrumo/macOS_Big_Sur_icons_replacements/master/icons/png/low-res/"+iconName+".png"
+                    "https://raw.githubusercontent.com/elrumo/macOS_Big_Sur_icons_replacements/master/icons/png/low-res/"+id+".png"
             }
             parent.iconList.push(itemObj)
             // console.log(itemObj)
@@ -122,6 +125,29 @@ export default {
           // csv.forEach(item=>{console.log(item)})
           })
     }
+  },
+
+  computed:{
+    filteredList: function () {
+
+      var iconList = this.iconList;
+      var searchString = this.searchString;
+
+      if(!searchString){
+        return iconList;
+      }
+      
+      searchString = searchString.trim().toLowerCase();
+
+      iconList = iconList.filter(function(item){
+        if(item.name.toLowerCase().indexOf(searchString) !== -1){
+          return item;
+        }
+      })
+
+      return iconList;
+    }
+
   },
 
   props: {
