de14854a6de01eff6711ae2c7f5c9fb2dcebb583
elrumo <elrumo97@me.com>
2020-08-04 15:37:45 +0200

    Full width sticky bar

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -3,17 +3,21 @@
     
     <!-- Header -->
     <div id="header" class="header">
-      <p class="header-item-left coral-Body--XS">Made by <a href="https://eliasruiz.com" target="_blank" class="coral-Link">Elias</a></p>
+      <p class="header-item-left coral-Body--S p-t-5">
+        Made by <a href="https://eliasruiz.com" target="_blank" class="coral-Link">Elias</a>
+      </p>
+      
       <p class="header-item-right coral-Body--XL"> 
         <a href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements" target="_blank" class="coral-Link"> 
           GitHub 
         </a>
       </p>
+
     </div>
     
     <!-- Hero -->
-    <div id="hero" class="p-b-50 m-b-20">
-      <h3 class="main-heading coral-Heading--XL coral-Heading--regular p-t-50">
+    <div id="hero" class="hero-wrapper">
+      <h3 class="main-heading coral-Heading--XL coral-Heading--regular">
         <span class="f-w-100 f-s-24">macOS Big Sur</span>
         <br>
         Replacements Icons
@@ -24,22 +28,30 @@
           <p> GitHub </p>
         </button>
 
-        <button is="coral-button" variant="primary" @click="getIconsArray" style="min-width:168px">
-          <p> Download all </p>
-        </button>
+        <a href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements/releases/download/v1.0/icons.zip">
+          <button is="coral-button" variant="primary" style="min-width:168px">
+            <p> Download all </p>
+          </button>
+        </a>
       </div>
 
     </div>
-    
+
     <!-- Icon Secion -->
     <section class="content-wrapper">
-    
-      <form class="coral-Form coral-Form--vertical m-auto main-search" style="max-width:300px;">
-        <coral-search class="w-full shadow" placeholder="Placeholder"></coral-search>
-      </form>
 
+      <div class="main-search-wrapper">
+        <div class="search-bg"></div>
+        <div class="coral-Form coral-Form--vertical m-auto main-search" style="max-width:300px;">
+          <div class="shadow main-border-radius">
+            <input v-model="search" type="text" is="coral-textfield" class="_coral-Search-input _coral-Textfield" name="name" aria-label="text input">
+            <svg class="icon" id="spectrum-css-icon-Magnifier" viewBox="0 0 16 16"><path d="M15.77 14.71l-4.534-4.535a6.014 6.014 0 1 0-1.06 1.06l4.533 4.535a.75.75 0 1 0 1.061-1.06zM6.5 11A4.5 4.5 0 1 1 11 6.5 4.505 4.505 0 0 1 6.5 11z"></path></svg>
+          </div>
+        </div>
+
+      </div>
+  
       <div class="icon-list-area p-t-50 p-b-50">
-      
         <a v-for="icon in iconList" :key="icon.name" :href="icon.url" class="card-wrapper shadow">
           <div class="card-img-wrapper">
             <img class="w-full" :src="icon.img" alt="">
@@ -54,6 +66,7 @@
 
     </section>
 
+    <!-- Footer -->
     <section>
       <footer class="p-b-20">
         Made with ❤️ by <a href="https://eliasruiz.com" target="_blank" class="coral-Link">Elias</a> 
@@ -63,18 +76,20 @@
 </template>
 
 <script>
+// import {Coral} from '@adobe/coral-spectrum'
+
 
 export default {
   name: 'Home',
 
   components: {
+    // Coral
   },
 
   data(){
     return{
-
-      iconList:[]
-      
+      iconList:[],
+      search: ""
     }
   },
 
