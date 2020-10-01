4c026b6096b7ee9638babf702c4e043c9324602b
Elias <elrumo97@me.com>
2020-10-01 20:03:10 +0100

    Fixed website colours and added lazy loading

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -30,21 +30,18 @@
         Replacement Icons
       </h3>
 
-
-      <p class="coral-Body--L p-t-20 w-100 body-text">
+      <p class="coral-Body--L w-100 body-text p-t-20">
         Click on each icon to download it or on the button bellow to download all {{iconList.length}} icons. To contribute or suggest a new icon, click on the GitHub button.
       </p>
+      
+      <a class="coral-Link" href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements" target="_blank">
+        GitHub
+      </a>
 
-      <div class="p-t-35 m-auto">
-        <a class="p-l-10" href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements" target="_blank">
-          <button is="coral-button" variant="quite" style="min-width:168px">
-            <p> GitHub </p>
-          </button>
-        </a>
-
-        <a class="p-l-10" href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements/releases/download/v1.0/icons.zip">
-          <button is="coral-button" variant="primary" style="min-width:168px">
-            <p> Download all </p>
+      <div class="m-auto m-t-30">
+        <a class="" href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements/releases/download/v1.0/icons.zip">
+          <button  is="coral-button" variant="cta">
+            <span>Download all</span>
           </button>
         </a>
       </div>
@@ -56,41 +53,27 @@
 
       <!-- Search bar -->
       <div class="main-search-wrapper">
-          <div id="search-bar" class="search-bg bg-light"></div>
         <div class="m-auto main-search" style="max-width:300px;">
           <div class="shadow main-border-radius">
             <input v-model="searchString" :placeholder="'Search ' + iconList.length + ' icons'" type="text"  class="_coral-Search-input _coral-Textfield" name="name" aria-label="text input">
-            <svg class="icon fill-dark" id="spectrum-css-icon-Magnifier" viewBox="0 0 16 16"><path d="M15.77 14.71l-4.534-4.535a6.014 6.014 0 1 0-1.06 1.06l4.533 4.535a.75.75 0 1 0 1.061-1.06zM6.5 11A4.5 4.5 0 1 1 11 6.5 4.505 4.505 0 0 1 6.5 11z"></path></svg>
+            <svg class="icon fill-dark" id="coral-css-icon-Magnifier" viewBox="0 0 16 16"><path d="M15.77 14.71l-4.534-4.535a6.014 6.014 0 1 0-1.06 1.06l4.533 4.535a.75.75 0 1 0 1.061-1.06zM6.5 11A4.5 4.5 0 1 1 11 6.5 4.505 4.505 0 0 1 6.5 11z"></path></svg>
           </div>
         </div>
-
       </div>
   
-      <div v-if="!darkMode" class="icon-list-area p-t-50 p-b-50">
-          <a v-for="icon in filteredList" :key="icon.name" class="card-wrapper shadow card-light" :href="icon.url">
+      <div class="icon-list-area p-t-50 p-b-50">
+          <a v-for="icon in filteredList" :key="icon.name" class="card-wrapper shadow coral-bg" :href="icon.url">
             <div class="card-img-wrapper">
-              <img loading="lazy" class="w-full" :src="icon.img" alt="">
+              <img loading="lazy" v-lazy="icon.img" class="w-full" alt="">
             </div>
             <div>
-              <h3 style="color: rgb(75, 75, 75)">
+              <h3 class="coral-font-color">
               {{ icon.name }}
               </h3>
             </div>
           </a>
       </div>
 
-      <div v-if="darkMode" class="icon-list-area p-t-50 p-b-50">
-          <a v-for="icon in filteredList" :key="icon.name+'-dark'" class="card-wrapper shadow card-dark" :href="icon.url">
-            <div class="card-img-wrapper">
-              <img loading="lazy" class="w-full" :src="icon.img" alt="">
-            </div>
-            <div>
-              <h3 style="color: white">
-              {{ icon.name }}
-              </h3>
-            </div>
-          </a>
-      </div>
 
     </section>
 
@@ -108,8 +91,6 @@
 </template>
 
 <script>
-// import {Coral} from '@adobe/coral-spectrum'
-
 
 export default {
   name: 'Home',
@@ -142,13 +123,10 @@ export default {
     toggleDarkMode(){
       let parent = this
       let body = document.getElementById("body")
-      let searchBar = document.getElementById("search-bar")
-      let searchIcon = document.getElementById("spectrum-css-icon-Magnifier")
+      let searchIcon = document.getElementById("coral-css-icon-Magnifier")
       
       body.classList.toggle('coral--light')
       body.classList.toggle('coral--dark')
-      searchBar.classList.toggle('bg-light')
-      searchBar.classList.toggle('bg-dark')
       searchIcon.classList.toggle('fill-light')
       searchIcon.classList.toggle('fill-dark')
       parent.darkMode = !parent.darkMode
@@ -207,12 +185,6 @@ export default {
           return item;
         }
       })
-
-      // for(let icon in iconList){
-      //   console.log(iconList[icon]);
-      // }
-      // console.log(iconList);
-
       return iconList;
     }
 
@@ -223,7 +195,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 @import url(app.css);
 @import url(snack-helper.min.css);
