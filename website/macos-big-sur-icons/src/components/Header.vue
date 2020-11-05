4a5b9ed1eb3dcfa46a6c5aa14a31c6ee5ad7ad81
Elias <36299870+elrumo@users.noreply.github.com>
2020-11-05 16:47:24 +0000

    fileName and icnsFileName props added to credits json object + dashboard now also shows approved icons + home is now connected to Submissions dir on Firestore DB

--- website/macos-big-sur-icons/src/components/Header.vue
+++ website/macos-big-sur-icons/src/components/Header.vue
@@ -1,19 +1,24 @@
 <template>
     <div id="header" class="header">
         <p class="header-item-left coral-Body--S p-t-5">
-        <a href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements#credits" target="_blank" class="coral-Link">Credits</a>
+        <a href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements" target="_blank" class="coral-Link">
+            GitHub
+        </a>
         </p>
         
         <p class="coral-Body--S p-t-5 d-inline-block absolute transform-centre">
-        Made by <a href="https://bit.ly/elias-webbites" target="_blank" class="coral-Link">Elias</a>
+        Made by <a href="https://webbites.io/" target="_blank" class="coral-Link">Elias</a>
         </p>
 
         <img v-if="darkMode" @click="toggleDarkMode" class="header-item-right dark-mode-btn" src="moon-light.svg" alt="dark-mode-btn">
         <img v-if="!darkMode" @click="toggleDarkMode" class="header-item-right dark-mode-btn" src="sun-dark.svg" alt="light-mode-btn">
 
         <p class="header-item-right coral-Body--XL github-header" id="github-header" > 
-        <a href="https://github.com/elrumo/macOS_Big_Sur_icons_replacements" target="_blank" class="coral-Link"> 
-            GitHub 
+        <a href="https://twitter.com/elrumo" target="_blank" class="coral-Link"> 
+            Twitter
+            <coral-icon size="S" alt="Smallest" title="XXS">
+                    <img :src="icons.twitter">
+            </coral-icon>
         </a>
         </p>
     </div>
@@ -26,6 +31,9 @@ export default {
     data(){
         return{
             darkMode: false,
+            icons:{
+                twitter: require("../assets/icons/twitter.svg"),
+            }
         }
     },
 
