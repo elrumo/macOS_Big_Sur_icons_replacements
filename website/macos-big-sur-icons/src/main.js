e93f4a411e8d7f100e3f82c202706f3b7cc68854
elrumo <elias.ruiz.monserrat@gmail.com>
2020-11-20 03:52:56 +0000

    Pagination added to Firestore data retrival, less calls to Firestore

--- website/macos-big-sur-icons/src/main.js
+++ website/macos-big-sur-icons/src/main.js
@@ -3,14 +3,12 @@ import App from './App.vue'
 import VueLazyload from 'vue-lazyload'
 import store from './store/store'
 import router from './router'
-import infiniteScroll from 'vue-infinite-scroll'
 
 Vue.config.productionTip = false
 Vue.use(VueLazyload, {
   preLoad: 1.3,
   loading: 'dist/loading.gif',
 });
-Vue.use(infiniteScroll);
 
 
 new Vue({
