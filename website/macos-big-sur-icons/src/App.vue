3dee59f2d2203fd1a20d95de1e50e1a62f23af65
elrumo <elias.ruiz.monserrat@gmail.com>
2020-12-06 20:49:58 +0100

    Dashboard animation removed

--- website/macos-big-sur-icons/src/App.vue
+++ website/macos-big-sur-icons/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Hi</h1>
     <router-view/>
   </div>
 </template>
@@ -9,19 +10,12 @@ import Home from './components/Home.vue'
 import '@adobe/coral-spectrum/dist/js/coral.js'
 import '@adobe/coral-spectrum/dist/css/coral.css'
 
-// import * as firebase from "firebase";
-// let firebaseConfig = {
-//   apiKey: process.env.VUE_APP_FIREBASE_KEY,
-//   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.VUE_APP_APP_ID,
-//   measurementId: process.env.VUE_APP_MEASUREMENT_ID,
-//   authDomain: "macos-icons.firebaseapp.com",
-//   databaseURL: "https://macos-icons.firebaseio.com",
-//   projectId: "macos-icons",
-//   storageBucket: "macos-icons.appspot.com"
-// };
-// firebase.initializeApp(firebaseConfig);
+import parse from 'parse'
+Parse.initialize("macOSicons");
+//javascriptKey is required only if you have it on server.
 
+Parse.serverURL = 'http://localhost:1337/parse'
+// console.log(parse);
 
 export default {
   name: 'App',
--- website/macos-big-sur-icons/src/store/store.js
+++ website/macos-big-sur-icons/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
