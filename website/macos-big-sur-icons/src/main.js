3645ec6b678f545156c6263fd5e1334dded56300
elrumo <elias.ruiz.monserrat@gmail.com>
2020-12-09 10:42:53 +0100

    ga fixed

--- website/macos-big-sur-icons/src/main.js
+++ website/macos-big-sur-icons/src/main.js
@@ -4,12 +4,12 @@ import App from './App.vue'
 import VueLazyload from 'vue-lazyload'
 import store from './store/store'
 import router from './router'
-import VueAnalytics from 'vue-analytics'
+// import VueAnalytics from 'vue-analytics'
 
-Vue.use(VueAnalytics, {
-  id: 'UA-46945907-10',
-  disabled: false,
-})
+// Vue.use(VueAnalytics, {
+//   id: 'UA-46945907-10',
+//   disabled: false,
+// })
 
 Vue.config.productionTip = false
 
