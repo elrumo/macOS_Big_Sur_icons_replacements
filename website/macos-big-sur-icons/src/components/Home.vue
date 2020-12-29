5dad5e7e04844cb77caa856b945f66672df1c01b
elrumo <elias.ruiz.monserrat@gmail.com>
2020-12-29 20:30:45 +0100

    Updated credentials

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -185,7 +185,7 @@ const VUE_APP_PARSE_MASTERKEY = process.env.VUE_APP_PARSE_MASTERKEY
 
 console.log(process.env);
 
-Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY, PARSE_MASTERKEY)
+Parse.initialize(VUE_APP_PARSE_APP_ID, VUE_APP_PARSE_JAVASCRIPT_KEY, VUE_APP_PARSE_MASTERKEY)
 Parse.serverURL = 'http://82.145.63.160:1337/parse'
 
 const Icons = Parse.Object.extend("Icons");
