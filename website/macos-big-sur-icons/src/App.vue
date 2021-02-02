37c62b57691f78f27a64e8bc8372e6d0158d95bc
elrumo <elias.ruiz.monserrat@gmail.com>
2021-02-02 16:17:08 +0100

    Improved performance by only using the coral components needed

--- website/macos-big-sur-icons/src/App.vue
+++ website/macos-big-sur-icons/src/App.vue
@@ -6,7 +6,19 @@
 
 <script>
 import Home from './components/Home.vue'
-import '@adobe/coral-spectrum/dist/js/coral.js'
+import '@adobe/coral-spectrum/coral-component-dialog'
+import '@adobe/coral-spectrum/coral-component-button'
+import '@adobe/coral-spectrum/coral-component-toast'
+import '@adobe/coral-spectrum/coral-component-icon'
+import '@adobe/coral-spectrum/coral-component-wait'
+import '@adobe/coral-spectrum/coral-component-progress'
+import '@adobe/coral-spectrum/coral-component-quickactions'
+import '@adobe/coral-spectrum/coral-component-fileupload'
+import '@adobe/coral-spectrum/coral-component-textfield'
+import '@adobe/coral-spectrum/coral-component-search'
+
+// import '@adobe/spectrum-css/dist/icons/loadIcons.js'
+// import '@adobe/spectrum-css/dist/components/link/index.css'
 import '@adobe/coral-spectrum/dist/css/coral.css'
 
 export default {
--- website/macos-big-sur-icons/src/router/index.js
+++ website/macos-big-sur-icons/src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/blog',
+    name: 'Blog',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Blog.vue')
+  },
+  {
     path: '/how-to',
     name: 'HowTo',
     // route level code-splitting
