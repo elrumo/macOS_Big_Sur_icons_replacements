effd494d7f8e63bdd19b490df16806e7d74cc23b
elrumo <elias.ruiz.monserrat@gmail.com>
2021-02-23 15:12:19 +0100

    Blog item added to header

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -412,15 +412,7 @@ export default {
         var id = icon.objectID
       }
 
-      console.log(id);
-      
-      // Get today's date
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, '0');
-      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-      var yyyy = today.getFullYear();
-
-      today = dd + '/' + mm + '/' + yyyy;
+      let today = parent.today;
 
       if (parent.isMacOs) {
         var platform = "macOS"
