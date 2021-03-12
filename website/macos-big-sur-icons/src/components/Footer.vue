2cbf47319939a37b661b7b86ed39ccfb156915e6
elrumo <elias.ruiz.monserrat@gmail.com>
2021-03-12 16:36:14 +0100

    rel='noopener' added to a tags

--- website/macos-big-sur-icons/src/components/Footer.vue
+++ website/macos-big-sur-icons/src/components/Footer.vue
@@ -1,10 +1,17 @@
 <template>
    <footer class="footer p-b-20 coral-Body--S">
-      Made with ❤️ by <a href="https://bit.ly/elias-webbites" target="_blank" class="coral-Link">Elias</a>
+      Made with ❤️ by <a href="https://bit.ly/elias-webbites" rel="noopener" target="_blank" class="coral-Link">Elias</a>
       <dir class="d-inline-block m-0 p-l-15 p-r-10">
         <hr class="coral-Divider--M coral-Divider--vertical m-0" style="height:14px;">
       </dir>
-      <a href="https://www.paypal.com/donate/?hosted_button_id=5PMNX4DPW83KN" target="_blank" class="coral-Link">Support the project</a>
+      <a
+        rel="noopener"
+        target="_blank"
+        href="https://www.paypal.com/donate/?hosted_button_id=5PMNX4DPW83KN"
+        class="coral-Link"
+      >
+          Support the project
+      </a>
     </footer>
 </template>
 
