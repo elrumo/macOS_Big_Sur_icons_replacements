8b08ff7c67190c0b7387b12db0449142c2df454f
elrumo <elias.ruiz.monserrat@gmail.com>
2021-03-17 21:08:07 +0000

    Temporary fix to ad issue

---
+++ website/macos-big-sur-icons/src/components/NativeAd.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="" id="iconbar-js"></div>
+</template>
+
+<script>
+
+export default {
+    name:"NativeAd",
+
+    directives:{},
+
+    components:{},
+    
+    data(){
+        return{
+        }
+    },
+
+    mounted: function(){
+      function getAd(){
+        if (typeof _bsa !== 'undefined' && _bsa) {
+          _bsa.init('custom', 'CESDC2QN', 'placement:macosiconscom',
+          {
+            target: '#iconbar-js',
+            template: `
+                <a href="##statlink##" target="_blank" rel="noopener sponsored" id="customAd" class="bsa-link">
+                <div class="bsa-icon" style="background-image: url(##image##); background-color: ##backgroundColor##;"></div>
+                <div class="bsa-desc">##company## - ##tagline##</div>
+                </a>
+              `
+            }
+          );
+        }
+      }
+
+      getAd()
+
+      setTimeout(() =>{
+      console.log("_bsa: ", _bsa);
+      console.log("_bsa: ", _bsa.callback(_bsa));
+        let el = document.getElementById("customAd")
+        if (!el) {
+          getAd()
+        }
+      }, 500)
+    },
+
+    props:{},
+
+    methods:{}
+}
+</script>
+
+<style>
+
+</style>
