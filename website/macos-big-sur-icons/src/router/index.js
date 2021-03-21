08babbfd9d7ac42012fb3f1620b5a52073852934
elrumo <elias.ruiz.monserrat@gmail.com>
2021-03-21 19:35:53 +0000

    Prender pages added to resources

--- website/macos-big-sur-icons/src/router/index.js
+++ website/macos-big-sur-icons/src/router/index.js
@@ -39,6 +39,53 @@ const routes = [
     component: BlogPost
   },
   {
+    path: '/blog/welcome-to-the-blog-3',
+    name: 'BlogPost-Welcome',
+    component: BlogPost,
+    meta: { 
+      title: 'Welcome to the macOSicons blog.',
+      description:"A brief retrospective of macOSicons.com, my future plans for the site and new upcoming features.",
+      titleTemplate: '%s | macOSicons',
+      meta:[
+        // Facebook
+        {
+          property: 'og:url',
+          content:  'https://macosicons.com/resources/welcome-to-the-blog-3'
+        },
+        {
+          property: 'og:title',
+          content:  'Welcome to the macOSicons blog.',
+        },
+        {
+          property: 'og:description',
+          content:  'A brief retrospective of macOSicons.com, my future plans for the site and new upcoming features.',
+        },
+        {
+          property: 'og:image',
+          content:  'https://raw.githubusercontent.com/elrumo/macOS_Big_Sur_icons_replacements/master/icons/Social/low-res/Resources-thumbnail.jpg'
+        },
+
+        // Twitter
+        {
+          property: 'twitter:url',
+          content:  'https://macosicons.com/resources'
+        },
+        {
+          property: 'twitter:description',
+          content:  'A brief retrospective of macOSicons.com, my future plans for the site and new upcoming features.',
+        },
+        {
+          property: 'twitter:title',
+          content:  'Welcome to the macOSicons blog.',
+        },
+        {
+          property: 'twitter:image',
+          content:  'https://raw.githubusercontent.com/elrumo/macOS_Big_Sur_icons_replacements/master/icons/Social/low-res/Resources-thumbnail.jpg'
+        },
+      ]
+    }
+  },
+  {
     path: '/how-to',
     name: 'HowTo',
     component: () => import('../views/HowTo.vue')
@@ -47,6 +94,100 @@ const routes = [
     path: '/forum',
     name: 'Forum',
     component: () => import('../views/Forum.vue')
+  },
+  {
+    path: '/resources/macos-big-sur-app-icon',
+    name: 'Resource-BigSur-template',
+    component: () => import('../views/ResourceView.vue'),
+    meta: { 
+      title: 'Free app icon template for macOS Big Sur.',
+      description:"Template to create app icons for macOS Big Sur. Contains instructions on how to upload your icons to macOSicons.com and a set of predefined styles ready for you to apply and edit.",
+      titleTemplate: '%s | macOSicons',
+      meta:[
+        // Facebook
+        {
+          property: 'og:url',
+          content:  'https://macosicons.com/resources'
+        },
+        {
+          property: 'og:title',
+          content:  'Free app icon template for macOS Big Sur.',
+        },
+        {
+          property: 'og:description',
+          content:  'Template to create app icons for macOS Big Sur. Contains instructions on how to upload your icons to macOSicons.com and a set of predefined styles ready for you to apply and edit.',
+        },
+        {
+          property: 'og:image',
+          content:  'https://blog.macosicons.com/blog/content/images/2021/03/intro-app-template-2.jpg'
+        },
+
+        // Twitter
+        {
+          property: 'twitter:url',
+          content:  'https://macosicons.com/resources'
+        },
+        {
+          property: 'twitter:description',
+          content:  'Template to create app icons for macOS Big Sur. Contains instructions on how to upload your icons to macOSicons.com and a set of predefined styles ready for you to apply and edit.',
+        },
+        {
+          property: 'twitter:title',
+          content:  'Free app icon template for macOS Big Sur.',
+        },
+        {
+          property: 'twitter:image',
+          content:  'https://blog.macosicons.com/blog/content/images/2021/03/intro-app-template-2.jpg'
+        },
+      ]
+    }
+  },
+  {
+    path: '/resources/macos-big-sur-dock',
+    name: 'Resource-BigSur-dock-template',
+    component: () => import('../views/ResourceView.vue'),
+    meta: { 
+      title: 'Free app icon Dock template for macOS Big Sur.',
+      description:"Figma template to show off your icons on the macOS Big Sur dock. Has dark and light variants and four different styles.",
+      titleTemplate: '%s | macOSicons',
+      meta:[
+        // Facebook
+        {
+          property: 'og:url',
+          content:  'https://macosicons.com/resources'
+        },
+        {
+          property: 'og:title',
+          content:  'Free app icon Dock template for macOS Big Sur.',
+        },
+        {
+          property: 'og:description',
+          content:  'Figma template to show off your icons on the macOS Big Sur dock. Has dark and light variants and four different styles.',
+        },
+        {
+          property: 'og:image',
+          content:  'https://blog.macosicons.com/blog/content/images/2021/03/intro_dock-2.jpg'
+        },
+
+        // Twitter
+        {
+          property: 'twitter:url',
+          content:  'https://macosicons.com/resources'
+        },
+        {
+          property: 'twitter:description',
+          content:  'Figma template to show off your icons on the macOS Big Sur dock. Has dark and light variants and four different styles.'
+        },
+        {
+          property: 'twitter:title',
+          content:  'Free app icon Dock template for macOS Big Sur.',
+        },
+        {
+          property: 'twitter:image',
+          content:  'https://blog.macosicons.com/blog/content/images/2021/03/intro_dock-2.jpg'
+        },
+      ]
+    }
   },
   {
     path: '/resources',
