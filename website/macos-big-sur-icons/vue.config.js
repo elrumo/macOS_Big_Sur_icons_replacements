199966c5773d877c78da12b5591bb53f4f7d2503
elrumo <elias.ruiz.monserrat@gmail.com>
2021-04-06 14:20:15 +0100

    Icon upload added back

--- website/macos-big-sur-icons/vue.config.js
+++ website/macos-big-sur-icons/vue.config.js
@@ -1,37 +1,11 @@
 const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
   configureWebpack:  {
     plugins:  process.env.NODE_ENV === 'production' ?[
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: [ 
-          '/',
-          '/WhatsApp',
-          
-          '/blog',
-          '/blog/welcome-to-the-blog-3',
-          '/blog/new-paid-access',
-
-          '/resources',
-          '/resources/macos-big-sur-app-icon',
-          '/resources/macos-big-sur-dock'
-        ],
-        
-        // renderAfterDocumentEvent: 'render-ready',
-        
-        renderer: new Renderer({
-          // Wait to render until the element specified is detected with document.querySelector.
-          headless: true, // Let's you watch what's happening.
-          renderAfterTime: 5000,
-
-          // renderAfterElementExists: '#app'
-        })
-      })
+      
     ] : []
   },
 
