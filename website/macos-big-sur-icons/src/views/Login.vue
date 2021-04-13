0f2cbdd6bc618946c3804ef670634f0937bf3a5d
elrumo <elias.ruiz.monserrat@gmail.com>
2021-04-14 00:05:57 +0100

    Login work started

--- website/macos-big-sur-icons/src/views/Login.vue
+++ website/macos-big-sur-icons/src/views/Login.vue
@@ -1,17 +1,45 @@
 <template>
-<div>
+  <div>
+        <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div>
 
+    <!-- <vue-apple-signin></vue-apple-signin> -->
+    <!-- <vue-apple-signin
+      style="cursor: pointer"
+      color="black"
+      :border="true"
+      type="sign in"
+    ></vue-apple-signin> -->
 
-</div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import Header from '@/components/Header.vue'
 
 export default {
   name: 'About',
+  
   components: {
+  },
+
+  mounted: function(){
+     
+     AppleID.auth.init({
+        clientId : process.env.VUE_APP_APPLE_CLIENTID,
+        scope : 'name email',
+        redirectURI : process.env.VUE_APP_APPLE_REDIRECT,
+        state : process.env.VUE_APP_APPLE_STATE,
+        usePopup : true //or false defaults to false
+    });
+ 
+
+    const singInApple = async () => {
+      const response = await window.AppleID.auth.signIn();
+      console.log(response);
+      return response;
+    };
+
+    singInApple()
+
   }
 }
 </script>
