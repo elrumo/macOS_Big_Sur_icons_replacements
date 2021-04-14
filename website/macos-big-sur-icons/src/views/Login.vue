ad9dfb3c04b360705c9deb5bba65585a14ded17b
elrumo <elias.ruiz.monserrat@gmail.com>
2021-04-14 21:56:00 +0100

    Twitter login started

--- website/macos-big-sur-icons/src/views/Login.vue
+++ website/macos-big-sur-icons/src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-        <div @click="appleLogin" style="height: 40px; width: 100px; background: black;" ></div>
+        <div @click="twitterLogin" style="height: 40px; width: 100px; background: black;" ></div>
+        <!-- <div @click="appleLogin" style="height: 40px; width: 100px; background: black;" ></div> -->
 
     <!-- <vue-apple-signin></vue-apple-signin> -->
     <!-- <vue-apple-signin
@@ -29,6 +30,15 @@ export default {
   },
   
   methods: {
+
+    async twitterLogin(){
+     
+
+
+
+
+    },
+
     async appleLogin(){
       let parent = this;
       const response = await window.AppleID.auth.signIn();
@@ -73,14 +83,21 @@ export default {
     //    }
     //  logIn()
 
-     AppleID.auth.init({
-        clientId : process.env.VUE_APP_APPLE_CLIENTID,
-        scope : 'email',
-        redirectURI : process.env.VUE_APP_APPLE_REDIRECT,
-        state : process.env.VUE_APP_APPLE_STATE,
-        usePopup : true //or false defaults to false
-    });
+    // AppleID.auth.init({
+    //   clientId : process.env.VUE_APP_APPLE_CLIENTID,
+    //   scope : 'email',
+    //   redirectURI : process.env.VUE_APP_APPLE_REDIRECT,
+    //   state : process.env.VUE_APP_APPLE_STATE,
+    //   usePopup : true //or false defaults to false
+    // });
 
+    AppleID.auth.init({
+      clientId : 'com.macOSicons.client',
+      scope : 'name email',
+      redirectURI : 'https://macosicons.com/redirect',
+      state : 'ZArDrecqhhR/ZMGzXa8XPqHWFJyfg8pV4gHiO/Pmv0Tz33YsYfDGheg2VC7KLwX+',
+      usePopup : true //or false defaults to false
+    });
 
 
   }
