97b0a51f832ed9c9535fe1804949879fa169bd57
elrumo <elias.ruiz.monserrat@gmail.com>
2021-05-11 01:12:18 +0100

    Fixsed some other minor visual bugs

--- website/macos-big-sur-icons/src/components/Home.vue
+++ website/macos-big-sur-icons/src/components/Home.vue
@@ -78,7 +78,7 @@
                       <rect id="ToDelete" fill="#ff13dc" opacity="0" width="12" height="12" /><path d="M11.69673,10.28266,7.41406,6l4.28267-4.28266A.9999.9999,0,1,0,10.28266.30327L6,4.58594,1.71734.30327A.9999.9999,0,1,0,.30327,1.71734L4.58594,6,.30327,10.28266a.9999.9999,0,1,0,1.41407,1.41407L6,7.41406l4.28266,4.28267a.9999.9999,0,1,0,1.41407-1.41407Z" />
                     </svg>
 
-                    <div class="mobile-hidden" style="top: -25px;">
+                    <div class="mobile-hidden" style="top: -26px;">
                       <button @click="copySearch" variant="quiet" is="coral-button">
                         Share search
                       </button>
