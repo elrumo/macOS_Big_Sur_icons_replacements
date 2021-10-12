9bb5571736f480e9c33ecb50b1c6c193e94d2c9b
Elias <36299870+elrumo@users.noreply.github.com>
2021-10-12 19:21:51 +0200

    Added transition to router changes

--- website/macos-big-sur-icons/src/components/EditIconDialog.vue
+++ website/macos-big-sur-icons/src/components/EditIconDialog.vue
@@ -143,17 +143,14 @@
 import { mapGetters, mapActions } from 'vuex'
 import Parse from 'parse'
 import UploadDialog from './UploadDialog.vue'
-
 Parse.initialize("macOSicons");
 Parse.serverURL = 'https://media.macosicons.com/parse'
-
 export default {
   name:"EditIconDialog",
   
   props:{
     icon: {}
   },
-
   components:{
     UploadDialog
   },
@@ -172,11 +169,8 @@ export default {
         type: "",
         icon: false,
       },
-
       toUpdate: {},
-
       uploadDialog: false,
-
       fileToUpload: {},
       fileToShow: {
         img: "",
@@ -192,10 +186,8 @@ export default {
       parent.toUpdate = Object.assign({}, parent.toUpdateTemplate);
     }
   },
-
   methods:{
     ...mapActions(['showToast', 'setUser', 'showEl']),
-
     resetDialog(){
       let parent  = this;
       
@@ -205,14 +197,11 @@ export default {
         img: "",
         name: ""
       }
-
     },
-
     async saveIconData(){
       let parent = this
       
       parent.isLoading = true
-
       var Icons = Parse.Object.extend("Icons2");
       let toUpdate = parent.toUpdate
       let iconQuery = new Parse.Query(Icons);
@@ -227,15 +216,12 @@ export default {
               parent.icon[key] = newValue
               icon.set(key, newValue)
               break;
-
             case "category":
               let categoryQuery = new Parse.Query(Parse.Object.extend("Categories"))
               let categoryParse = await categoryQuery.get(newValue)
-
               parent.icon[key] = categoryParse
               icon.set(key, categoryParse)
               break;
-
             case "type":
               let typeQuery = new Parse.Query(Parse.Object.extend("IconType"))
               let typeParse = await typeQuery.get(newValue)
@@ -247,11 +233,9 @@ export default {
             default:
               break;
           }          
-
         }
         // ParseUser.set(key, toUpdate[key])
       }
-
       if(parent.imageData){
         icon = await parent.onUplaod(icon);
         parent.icon.highResPngUrl = icon.get('highResPngUrl')
@@ -265,28 +249,22 @@ export default {
         
         // Reset edit dialog
         parent.resetDialog()
-
         parent.showToast({
           id: "toastMessage",
           message: parent.icon.appName+" updated.",
           variant: "success"
         })
-
       }).catch((error) => {
         parent.isLoading = false
         console.log(error);
-
         document.getElementById("editIconDialog").hide()
         parent.showToast({
           id: "toastMessage",
           message: "Something went wrong, please try again.",
           variant: "error"
         })
-
       })
-
     },
-
     iconUrl(icon){
         if (!icon.lowResPngUrl) {
             return icon.highResPngUrl
@@ -294,7 +272,6 @@ export default {
             return icon.lowResPngUrl
         }
     },
-
     isSameCategory(field, category){
       let parent = this
       let icon = parent.icon[field]
@@ -308,7 +285,6 @@ export default {
         return false
       }
     },
-
     selectedOption(option, value){
       if (option == value) {
         return true
@@ -316,21 +292,17 @@ export default {
         return false
       }
     },
-
     getValue(e, field){
       let parent = this
       let target = e.target
       let toUpdate = parent.toUpdate
       let fieldValue = target.value
-
       toUpdate[field] = fieldValue;
     },
-
     setYourName(e){
       console.log(e.target.value);
       this.yourName = e.target.value
     },
-
     propExists(prop){
       let ParseUser = Parse.User.current()
       if (!ParseUser) {
@@ -340,19 +312,15 @@ export default {
         return ParseUser[prop]
       }
     },
-
     validate(e, field){
       let parent = this
       try {
         let isInvalid = e.target.classList.contains("is-invalid")
-
         if (isInvalid) { parent.isValidated = false }
         else{ parent.isValidated = true }
-
       }catch(error){ }
       parent.getTextFieldValue(e, field)
     },
-
     getTextFieldValue(e, field){
       let parent = this
       let fieldValue
@@ -365,16 +333,13 @@ export default {
       parent.hasChanged = true
       parent.toUpdate[field] = fieldValue
     },
-
     updateUserProp(){
       let parent = this
       let toUpdate = parent.toUpdate
       let ParseUser = Parse.User.current()
-
       for(let key in toUpdate){
         ParseUser.set(key, toUpdate[key])
       }
-
       ParseUser.save().then((data) =>{
         parent.showToast({
           id: "toastMessage",
@@ -385,7 +350,6 @@ export default {
         console.log(error);
       })
     },
-
     getUserData(prop){
       let parent = this
       let ParseUser = Parse.User.current()
@@ -393,10 +357,8 @@ export default {
       // console.log(prop, " :", userProps[prop]);
       return userProps[prop]
     },
-
     isValid(field){
       let parent = this
-
       try {
         let icon = parent.toUpdate[field]
         // console.log(field, ": ", icon);
@@ -407,9 +369,7 @@ export default {
           icon[0] != " " &&
           icon.length > 1
         ){ return true } else{ return false }        
-
       } catch (error) {}
-
     },
     
     selectIcon(event) {
@@ -431,16 +391,13 @@ export default {
       // parent.$set(parent.fileToShow, fileName, value)
       parent.fileToShow.img = objectURL
       parent.fileToShow.name = fileName
-
       parent.imageData = true
     },
-
     async onUplaod(icon){
       let parent = this;
       let file = parent.fileToUpload;
       let fileName = file.name.replace('.png', '');
       let userName = parent.$store.state.user.username;
-
       if (/^[A-Za-z][A-Za-z0-9]*$/.test(fileName)) {
         fileName = fileName
       } else {
@@ -453,44 +410,33 @@ export default {
       if (/^[A-Za-z][A-Za-z0-9]*$/.test(userName)) {
         fileName = fileName + '_' + userName
       }
-
       const Icons = Parse.Object.extend("Icons2"); 
       const parseFile = new Parse.File(fileName, file); 
       
       let savedFile = await parseFile.save()
-
       await icon.set("highResPngUrl", savedFile._url);
       await icon.set("isReUpload", true);
       await icon.set("isReview", false);
       await icon.set("approved", false);
       await icon.set("highResPngFile", parseFile);
-
       return icon
-
       // console.log(parseFile);
     }
-
-
   },
-
   mounted: async function(){
     let parent = this; 
     const store = parent.$store; 
-
     // Copies the value of the object instead of assigning a reference
     // parent.toUpdate = Object.assign({}, parent.icon);
     parent.toUpdate = Object.assign({}, parent.toUpdateTemplate)
-
     let isAuth = store.getters.getUser.isAuth
     if (!isAuth) {
       console.log("Not logged in");
     } else{
     }
   },
-
   computed:{
     ...mapGetters(['getUser', 'getAppCategories', 'getIconType']),
-
     hasChanged(){
       let parent = this
       let icon = parent.icon
@@ -512,15 +458,11 @@ export default {
         } else{
           return false
         }
-
       } catch (error) {
         console.log("error: ", error);
       }
-
     },
-
   }
-
 }
 </script>
 
