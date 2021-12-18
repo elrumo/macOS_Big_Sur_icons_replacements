46c88efc6ed4dd88a920606c61c5cd6076d55620
Elias Ruiz Monserrat <elias@ibm.com>
2021-12-18 12:43:13 +0100

    Added back eslint

---
+++ website/macos-big-sur-icons/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:vue/essential",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
