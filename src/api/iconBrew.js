0de75e7cd112697a9c07a2abd83daf39849174ab
Elias Ruiz Monserrat <elrumo97@me.com>
2022-03-05 15:54:58 +0000

    Started using IconBrew.com icons + fixed issue where the title of an icon could not be changed

--- src/api/iconBrew.js
+++ src/api/iconBrew.js
@@ -1,5 +1,17 @@
-import alertWithCircle from "../assets/icons/iconBrew/alert-with-circle.svg"
+import alertWithCircle24 from "../assets/icons/iconBrew/24px/alert-with-circle.svg"
+import uploadToCloud24 from "../assets/icons/iconBrew/24px/upload-to-cloud.svg"
+import trashWithLines24 from "../assets/icons/iconBrew/24px/trash-with-lines.svg"
+
+import alertWithCircle18 from "../assets/icons/iconBrew/18px/alert-with-circle.svg"
+import uploadToCloud18 from "../assets/icons/iconBrew/18px/upload-to-cloud.svg"
+import trashWithLines18 from "../assets/icons/iconBrew/18px/trash-with-lines.svg"
 
 export default {
-    alertWithCircle,
+    alertWithCircle24,
+    uploadToCloud24,
+    trashWithLines24,
+    
+    alertWithCircle18,
+    uploadToCloud18,
+    trashWithLines18,
 }
